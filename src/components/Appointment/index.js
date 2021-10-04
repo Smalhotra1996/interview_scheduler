e7fe37d3a64e81f4be31c6a9217e1a47daaa59e7
Smalhotra1996 <sonam.malhotra2496@gmail.com>
2021-10-04 18:30:39 -0400

    Worked on saving interviews

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,24 +4,36 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
-
+import Status from "./Status";
 import "./styles.scss";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  const save = (name, interviewer) => {
+    transition(SAVING);
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW));
+  }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
-      {mode === CREATE && <Form  onCancel={()=> back()} interviewers={props.interviewers} />}
+      {mode === CREATE && <Form onCancel={() => back()} interviewers={props.interviewers} onSave={(name, interviewer) => save(name, interviewer)} />}
+      {mode === SAVING && <Status message={"SAVING"}/>}
     </article>
   );
 }
